be77a33730f99cfc86164ee1952457d9d5101a7c
simonTemplarFilipMorris <msb.acer@gmail.com>
2024-12-10 09:56:20 +0100

    Primer commi2t

--- cars_reviews/settings.py
+++ cars_reviews/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 #DATABASE_URL = 'postgresql://postgres:dEABbCgyqqXRNQvxJZwlKeFtzcCjYFSg@postgres.railway.internal:5432/railway'
-DATABASE_URL = 'postgresql://postgres:dEABbCgyqqXRNQvxJZwlKeFtzcCjYFSg@autorack.proxy.rlwy.net:43106/railway'
+DATABASE_URL = 'postgresql://postgres:nzfvVdaTXfiOUImwCxyMNqKunYyuysGH@autorack.proxy.rlwy.net:41705/railwayy'
 
 
 # Quick-start development settings - unsuitable for production
@@ -32,17 +32,17 @@ DEBUG = True
 
 #para desplegar en railway hay que descomentar esto de abajo y para local hay que comentarlo
 
-#CSRF_TRUSTED_ORIGINS = ['https://web-production-0c64.up.railway.app']
-#SESSION_COOKIE_SECURE = True
-#CSRF_COOKIE_SECURE = True
-#SECURE_SSL_REDIRECT = True
-#USE_X_FORWARDED_HOST = True
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = ['https://web-production-85c59.up.railway.app']
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-#ALLOWED_HOSTS = ['web-production-0c64.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-85c59.up.railway.app', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -94,27 +94,27 @@ WSGI_APPLICATION = 'cars_reviews.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'dEABbCgyqqXRNQvxJZwlKeFtzcCjYFSg',
+        'PASSWORD': 'nzfvVdaTXfiOUImwCxyMNqKunYyuysGH',
         'HOST': 'postgres.railway.internal',
         'PORT': '5432',
     }
 }
-"""
+
 
 """ DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL)
